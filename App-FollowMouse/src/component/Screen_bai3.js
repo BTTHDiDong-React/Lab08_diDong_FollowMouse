ab1571cb2241c2628c731382d5d11b6b1e3bcffd
nguyenvangithub <81097647+nguyenvangithub@users.noreply.github.com>
2022-11-09 09:46:26 +0700

    screen 3: finish screen(1 cat -  3 mouse)

--- App-FollowMouse/src/component/Screen_bai3.js
+++ App-FollowMouse/src/component/Screen_bai3.js
@@ -1,54 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import { useRef, useState } from 'react';
-import { Animated, StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+import { useEffect, useRef, useState } from 'react';
+import { Animated, FlatList, StatusBar, StyleSheet, Text, View } from 'react-native';
 import { Button, Image } from 'react-native';
-import icon from '../images/cat.webp'
 
+import styles from '../themes/styles';
 
 export default function Screen_bai3() {
 
-    const [location, setLocation]
-        = useState({
-            x: null,
-            y: null,
-            marginLeft: new Animated.Value(10),
-            marginTop: new Animated.Value(10)
+    let IMAGE_MOUSE = require('../images/mouse.webp')
+    let IMAGE_CAT = require('../images/cat.webp')
+    // RandDom
+    let randomX = () => { // chieu ngang
+        return Math.floor(Math.random() * (250 - 1)) + 1;
+    }
+    let randomY = () => { // chieu doc
+        return Math.floor(Math.random() * (470 - 1)) + 1;
+    }
+    // vi tri cat
+    const [locationCat, setLocationCat] = useState({
+        x: null,
+        y: null,
+        fakeLeft: useRef(new Animated.Value(120)).current,
+        fakeTop: useRef(new Animated.Value(10)).current,
+    })
+    // vi tri mouse
+    const [locationM1, setLocationM1] = useState({
+        fakeLeft: useRef(new Animated.Value(randomX())).current,
+        fakeTop: useRef(new Animated.Value(randomY())).current,
+    })
+    const [locationM2, setLocationM2] = useState({
+        fakeLeft: useRef(new Animated.Value(randomX())).current,
+        fakeTop: useRef(new Animated.Value(randomY())).current,
+    })
+    const [locationM3, setLocationM3] = useState({
+        fakeLeft: useRef(new Animated.Value(randomX())).current,
+        fakeTop: useRef(new Animated.Value(randomY())).current,
+    })
 
-        });
-
-    function onPress(evt) {
 
 
-        console.log('====================================');
-        var x = evt.nativeEvent.locationX;
-        console.log('====================================');
-        console.log(x);
-        console.log('====================================');
-        var y = evt.nativeEvent.locationY;
-        console.log('====================================');
-        setLocation({
-            x: x,
-            y: y,
-            marginLeft: x,
-            marginTop: y
 
-        })
+    //
+    const onPress = (evt) => {
+        console.log("🚀 ~ file: Screen_bai3.js ~ line 13 ~ onPress ~ evt");
 
+        let x = evt.nativeEvent.locationX;
+        let y = evt.nativeEvent.locationY;
 
     }
-    function onMove(evt) {
+    //
+    const onMove = (evt) => {
+        console.log("🚀 ~ file: Screen_bai3.js ~ line 21 ~ onMove ~ onMove (🤠)")
 
-
-
-        console.log('====================================');
-        console.log(location);
-        console.log('====================================');
-        //setLocation({marginLeft: x, marginTop: y })
     }
-    function onRelease() {
-        console.log("Realse!");
+    //
+    const onRelease = () => {
+        console.log("🚀 ~ file: Screen_bai3.js ~ line 26 ~ onRelease ~ onRelease(😎)")
+
     }
-    const { marginTop, marginLeft } = location;
+
     return (
         <View
             onStartShouldSetResponder={() => true}
@@ -56,24 +66,51 @@ export default function Screen_bai3() {
             onResponderGrant={onPress}
             onResponderMove={onMove}
             onResponderRelease={onRelease}
-            style={styles.container}>
-
-            <Text>Show something!</Text>
+            style={[styles.flex_1, { backgroundColor: '#ffb133' }]}>
+            <StatusBar />
 
             <Animated.Image
-                source={icon}
-                style={{ marginLeft: marginLeft, marginTop: marginTop }}
-            >
+                source={IMAGE_CAT} style={[, {
+                    width: 100, height: 100,
+                    position: 'absolute',
+                    top: locationCat.fakeTop,
+                    left: locationCat.fakeLeft
+                }]}
+            />
+            
 
-            </Animated.Image>
+
+            <Animated.Image
+                source={IMAGE_MOUSE} style={[, {
+                    width: 60, height: 60,
+                    position: 'absolute',
+                    top: locationM1.fakeTop,
+                    left: locationM1.fakeLeft,
+                    // backgroundColor: '#0f0'
+                }]}
+            />
+            <Animated.Image
+                source={IMAGE_MOUSE} style={[, {
+                    width: 60, height: 60,
+                    position: 'absolute',
+                    top: locationM2.fakeTop,
+                    left: locationM2.fakeLeft,
+                    // backgroundColor: '#0f0'
+                }]}
+            />
+            <Animated.Image
+                source={IMAGE_MOUSE} style={[, {
+                    width: 60, height: 60,
+                    position: 'absolute',
+                    top: locationM3.fakeTop,
+                    left: locationM3.fakeLeft,
+                    // backgroundColor: '#0f0'
+                }]}
+            />
+
 
         </View>
     );
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1
 
-    },
-});
